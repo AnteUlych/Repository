82b7cd251042ea706f83048150e3d64c00f13c13
AnteUlych <satoru@i.ua>
2017-11-26 20:54:33 +0200

    + .jsp for clients with no cargo

--- bluebird/src/main/java/bird/web/ClientController.java
+++ bluebird/src/main/java/bird/web/ClientController.java
@@ -18,81 +18,95 @@ import bird.expediter.Expediter;
 import bird.model.Cargo;
 import bird.model.Route;
 
-
 @Controller
 @RequestMapping("/cabinet")
 public class ClientController {
-	
-	Expediter monitoring = new Expediter();	
-	
+
+	Expediter monitoring = new Expediter();
+
 	@RequestMapping(method = RequestMethod.GET)
 	public String showQuote(ModelMap model, HttpServletRequest request) {
-		
-		HttpSession session = request.getSession();
-		
-		String clientId = (String) session.getAttribute("clientID");
-		int clientID = Integer.parseInt(clientId);
-		
-		String CargoId = (String) session.getAttribute("cargoID");
-		int cargoID = Integer.parseInt(CargoId);
-		
-		if(cargoID == 0){
-			cargoID = monitoring.getNewestCargo(clientID);
-			session.setAttribute("cargoID",cargoID+"");
+
+		try { // for client with no crgo redirect to page with no map
+
+			HttpSession session = request.getSession();
+
+			String clientId = (String) session.getAttribute("clientID");
+			int clientID = Integer.parseInt(clientId);
+
+			String CargoId = (String) session.getAttribute("cargoID");
+			int cargoID = Integer.parseInt(CargoId);
+
+			if (cargoID == 0) {
+				cargoID = monitoring.getNewestCargo(clientID);
+				session.setAttribute("cargoID", cargoID + "");
+			}
+
+			Cargo actualCargo = monitoring.getCargoBy(cargoID);
+
+			DateFormat eddformat = new SimpleDateFormat("dd/MM/yyyy");
+			String generalInformation = actualCargo.getDescription()
+					+ "; estimated delivery date: "
+					+ eddformat.format(actualCargo.getDelivery());
+			List<Cargo> cargoes = monitoring.getActiveCargoesByClient(clientID);
+			List<Route> waybill = monitoring.getRoutebyCargoId(cargoID);
+			String way = monitoring.getStatisticForMap(cargoID);
+			// String way
+			// ="[['<center>10:00 24/10/2017</center><center>XX 1111 XX</center>', 51.508742,-0.120850,],['<center>10:00 25/10/2017</center><center>XX 1111 XX</center>', 52.395715,4.888916,],['<center>10:00 26/10/2017</center><center>XX 1111 XX</center>', 52.340748, 20.478142,]]";
+			model.addAttribute("client", actualCargo.getClient());
+			model.addAttribute("generalInformation", generalInformation);
+			model.addAttribute("cargoes", cargoes);
+			model.addAttribute("waybill", waybill);
+
+			model.addAttribute("way", way);
+
+			model.addAttribute("needComment",
+					monitoring.isCargoCommentExists(cargoID));
+
+			model.addAttribute("lastUpdate",
+					monitoring.getTimeOfLastUpdateByClient(clientID));
+			model.addAttribute("totalCargoes",
+					monitoring.getTotalCargoesByClient(clientID));
+
+		} catch (IndexOutOfBoundsException ioobe) {
+			return "EmptyPage";
 		}
-		Cargo actualCargo = monitoring.getCargoBy(cargoID);
-		DateFormat eddformat = new SimpleDateFormat("dd/MM/yyyy");
-		String generalInformation = actualCargo.getDescription()+"; estimated delivery date: "+eddformat.format(actualCargo.getDelivery());
-		List<Cargo> cargoes = monitoring.getActiveCargoesByClient(clientID);
-        List<Route> waybill = monitoring.getRoutebyCargoId(cargoID);
-        String way = monitoring.getStatisticForMap(cargoID);
-        //String way ="[['<center>10:00 24/10/2017</center><center>XX 1111 XX</center>', 51.508742,-0.120850,],['<center>10:00 25/10/2017</center><center>XX 1111 XX</center>', 52.395715,4.888916,],['<center>10:00 26/10/2017</center><center>XX 1111 XX</center>', 52.340748, 20.478142,]]"; 
-        model.addAttribute("client", actualCargo.getClient());
-        model.addAttribute("generalInformation", generalInformation);
-		model.addAttribute("cargoes", cargoes);
-		model.addAttribute("waybill", waybill);
-		
-		model.addAttribute("way", way);
-		
-		model.addAttribute("needComment", monitoring.isCargoCommentExists(cargoID));
-			
-		model.addAttribute("lastUpdate", monitoring.getTimeOfLastUpdateByClient(clientID));
-		model.addAttribute("totalCargoes", monitoring.getTotalCargoesByClient(clientID));
-		
-		
-	return "Cabinet";
+
+		return "Cabinet";
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST)
-	public String post(ModelMap model, HttpServletRequest request, HttpServletResponse response) {
-		
+	public String post(ModelMap model, HttpServletRequest request,
+			HttpServletResponse response) {
+
 		HttpSession session = request.getSession();
-		
+
 		String clientId = (String) session.getAttribute("clientID");
-		String cargoId = (String) session.getAttribute("cargoID"); 
+		String cargoId = (String) session.getAttribute("cargoID");
 		int clientID = Integer.parseInt(clientId);
-		
+
 		List<Cargo> cargoes = monitoring.getActiveCargoesByClient(clientID);
-          
+
 		model.addAttribute("cargoes", cargoes);
-		
+
 		String forward = "notion";
-		
-		for(Cargo c:cargoes){		
-		if (request.getParameter("find"+cargoes.indexOf(c)) != null) {
-			int cargoID = Integer.parseInt(request.getParameter("cargoid"+cargoes.indexOf(c)));
-			session.setAttribute("cargoID",cargoID+"");
-			session.setAttribute("clientID",clientId);
-			try {
-				response.sendRedirect("/bluebird/gsp");
-				forward = "Cabinet";
-				return forward;
-			} catch (IOException e) {
-				e.printStackTrace();
+
+		for (Cargo c : cargoes) {
+			if (request.getParameter("find" + cargoes.indexOf(c)) != null) {
+				int cargoID = Integer.parseInt(request.getParameter("cargoid"
+						+ cargoes.indexOf(c)));
+				session.setAttribute("cargoID", cargoID + "");
+				session.setAttribute("clientID", clientId);
+				try {
+					response.sendRedirect("/bluebird/gsp");
+					forward = "Cabinet";
+					return forward;
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		}
-		}
-		//!
+		// !
 		if (request.getParameter("comment") != null) {
 			try {
 				response.sendRedirect("/bluebird/notion");
@@ -101,13 +115,9 @@ public class ClientController {
 				e.printStackTrace();
 			}
 		}
-		//!
-		
+		// !
+
 		return forward;
 	}
 
-		
-	}
-	
-
-
+}
