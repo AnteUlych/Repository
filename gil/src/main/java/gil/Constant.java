015c28ca2b3636ce27f00ab42ee50c04a59cc0a5
AnteUlych <satoru@i.ua>
2018-02-20 21:39:17 +0200

    add gil.jar (monitoring of site availability)

---
+++ gil/src/main/java/gil/Constant.java
@@ -0,0 +1,11 @@
+package gil;
+
+public interface Constant {
+
+	public static final String NAME = "uplg.info";
+	public static final String SITE = "http://uplg.info/tracing/top";
+	public static final int OK = 200;
+	public static final int TIME = 300000; // 5 minutes
+	public static final String ADMIN = "anton.ulych@gmail.com";
+
+}
---
+++ gil/src/main/java/gil/Main.java
@@ -0,0 +1,13 @@
+package gil;
+
+public class Main {
+
+	// program creating for monitoring of sites
+
+	public static void main(String[] args) {
+
+		Functional gil = new Functional();
+		gil.start();
+
+	}
+}
