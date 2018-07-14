8bf3d600ad9a6431ab19f58b17b941b400a45678
AnteUlych <satoru@i.ua>
2018-07-14 22:09:08 +0300

    monitoring: sendMethod
    +sendMethod

--- allay_monitoring/src/main/java/beagle/temp/SimpleTest.java
+++ allay_monitoring/src/main/java/beagle/temp/SimpleTest.java
@@ -1,23 +1,16 @@
 package beagle.temp;
 
-import java.util.Date;
-import java.util.List;
 
-import beagle.dispatcher.Service;
-import beagle.model.Booking;
-import beagle.model.Client;
-import beagle.model.Manager;
+import beagle.dispatcher.Sender;
+
 
 public class SimpleTest {
 
 	public static void main(String[] args) {
 
-		Service service = new Service();
-		System.out.println(service.getManagerByName("Inna").getPhone());
-		//List<String> updates = service.getAllManagers();
-		//for(String u:updates){
-		//System.out.println(u);
-		//}
+		Sender bird = new Sender("Anton", "satoru@i.ua", "satoru@i.ua", "xzx", 50, 15, "status", "etd", "update");
+		bird.sendMonitoring();
+		System.out.println("done");
 	}
 
 }
